9ac1ba079e3d6f5c810deb2082a12fc2e07eed8f
Julia Nikolaeva <julia.matveeva@gmail.com>
2018-02-08 21:28:55 +0300

    Graceful shutdown implemented

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"net/http"
-	"github.com/arahna/go-course/handlers"
+	"github.com/arahna/simple-video-service/handlers"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"context"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -13,11 +16,41 @@ func main() {
 	if err == nil {
 		log.SetOutput(file)
 	}
-
 	defer file.Close()
 
-	serverUrl := ":8000"
+	killSignalChan := getKillSignalChan()
+	srv := startServer(":8000")
+
+	waitForKillSignal(killSignalChan)
+	srv.Shutdown(context.Background())
+}
+
+func startServer(serverUrl string) *http.Server {
 	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
+
 	router := handlers.Router()
-	log.Error(http.ListenAndServe(serverUrl, router))
+	srv := &http.Server{Addr: serverUrl, Handler: router}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	return srv
+}
+
+func getKillSignalChan() chan os.Signal {
+	osKillSignalChan := make(chan os.Signal, 1)
+	signal.Notify(osKillSignalChan, os.Kill, os.Interrupt, syscall.SIGTERM)
+	return osKillSignalChan
+}
+
+func waitForKillSignal(killSignalChan <- chan os.Signal) {
+	killSignal := <- killSignalChan
+
+	switch killSignal {
+	case os.Interrupt:
+		log.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("got SIGTERM...")
+	}
 }
